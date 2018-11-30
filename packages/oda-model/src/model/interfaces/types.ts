b47eb110ca095596a21d4e47437054be59586855
vedanta-krit.das <vedmalex@gmail.com>
2018-11-30 18:01:12 +0300

    types

--- packages/oda-model/src/model/interfaces/types.ts
+++ packages/oda-model/src/model/interfaces/types.ts
@@ -1,6 +1,96 @@
+export type Nullable<T> = { [P in keyof T]: T[P] | undefined | null };
+
+/**
+ *
+ */
+export type assignInput<I, T, K = any> = {
+  /**
+   * source object
+   */
+  src: I;
+  /**
+   * dest object
+   */
+  input: Nullable<T>;
+  /**
+   * field from source object which is assigned
+   */
+  field?: keyof I | keyof I & keyof T;
+  /**
+   * function that will call to assign value
+   */
+  effect?: (src: I, value: K) => void;
+  /**
+   * function that check if effect is allowed
+   */
+  allowEffect?: (src: I, value: K) => boolean;
+  /**
+   * is field required
+   */
+  required?: boolean;
+  /**
+   * field from dest object which value will be assigned to dest field
+   */
+  inputField?: keyof T;
+  /**
+   * function that will call to set default value
+   */
+  setDefault?: (src: I) => void;
+};
+
+/**
+ * check specified value from input and assign
+ * @param options assign Options
+ */
+export function assignValue<I, T, K = any>(options: assignInput<I, T, K>) {
+  let {
+    src,
+    input,
+    field,
+    effect,
+    required,
+    inputField,
+    setDefault,
+    allowEffect,
+  } = options;
+
+  if (!inputField) {
+    inputField = field as keyof T;
+  }
+  if (input.hasOwnProperty(inputField)) {
+    if (input[inputField] != null) {
+      if (effect) {
+        if (
+          (allowEffect && allowEffect(src, input[inputField] as any)) ||
+          !allowEffect
+        ) {
+          effect(src, input[inputField] as any);
+        }
+      } else if (field) {
+        src[field] = input[inputField] as any;
+      }
+    } else if (field && src[field] == null && required) {
+      throw new Error("can't reset required value");
+    } else if (field && src[field] === null) {
+      if (setDefault instanceof Function) {
+        setDefault(src);
+      } else {
+        delete src[field];
+      }
+    }
+  } else {
+    if (field && required && src[field] == null) {
+      if (setDefault instanceof Function) {
+        setDefault(src);
+      }
+    }
+  }
+}
+
 export type RelationType = 'HasMany' | 'HasOne' | 'BelongsToMany' | 'BelongsTo';
 
 export type MetaModelType =
+  | 'element'
   | 'metadata'
   | 'model'
   | 'query'
